eaabdb5a1ed58a16f69d8294d8c0889790da88ae
Serhii Matkobozhyk <megawacko94@gmail.com>
2024-01-22 21:05:23 +0200

    finished homework 3

---
+++ jsBasics/task4.js
@@ -0,0 +1,11 @@
+const personAge = 18;
+let isAdult;
+
+if (personAge >= 18) {
+    isAdult = true;
+} else {
+    isAdult = false;
+}
+
+console.log(personAge);
+console.log(`Adult person: ${isAdult}`);
---
+++ jsBasics/task5.js
@@ -0,0 +1,16 @@
+// 5.1
+const radius = 10;
+const circleArea = Math.PI * Math.pow(radius, 2);
+console.log(circleArea.toFixed(2));
+
+// 5.2
+const rectangleLength = 10;
+const rectangleWidth = 5.51;
+const rectangleArea = rectangleLength * rectangleWidth;
+console.log(rectangleArea.toFixed(2));
+
+// 5.3
+const cylinderRadius = 5;
+const cylinderHeight = 20;
+const cylinderVolume = Math.PI * Math.pow(cylinderRadius, 2) * cylinderHeight;
+console.log(cylinderVolume.toFixed(2));
