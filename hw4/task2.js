78591fef438626e5173aa2432f95d29c19175de8
Serhii Matkobozhyk <megawacko94@gmail.com>
2024-01-26 19:08:59 +0200

    Finished hw4

---
+++ hw4/task2.js
@@ -0,0 +1,22 @@
+const averageGrade = 0;
+
+switch(!!averageGrade) {
+    case false:
+    case averageGrade > 0 && averageGrade < 60:
+        console.log('Незадовільно');
+        break
+    case averageGrade >= 60 && averageGrade <= 70:
+        console.log('Задовільно');
+        break
+    case averageGrade > 70 && averageGrade <= 80:
+        console.log('Добре');
+        break
+    case averageGrade > 80 && averageGrade <= 90:
+        console.log("Дуже добре");
+        break
+    case averageGrade > 90 && averageGrade <= 100:
+        console.log('Відмінно');
+        break
+    default:
+        console.log('Оцінка не правильна!');
+}
